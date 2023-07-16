8a39404890b1a29fa284ba859c6a2c056d42c420
Younis ahmed <younisalx-swe@hotmail.comgit config --global user.email younisalx-swe@hotmail.comgit config --global user.email younisalx-swe@hotmail.com>
2023-07-16 00:29:31 -0700

    private attr in class rectangle

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,11 @@
+#!/us/bin/python3
+
+class Rectangle(Base):
+
+
+   def __init__(self, width, height, x=0, id=None):
+      super()__init__(id)
+      self.__width = width
+      self.__height = height
+      self.__x = x
+
