baa935fb806a4763ca107b6f630b2b1a1e655972
Younis ahmed <younisalx-swe@hotmail.comgit config --global user.email younisalx-swe@hotmail.comgit config --global user.email younisalx-swe@hotmail.com>
2023-09-14 04:15:12 -0700

    minor fixes

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 """
 lists all states with a name starting with N (upper N)
 from the database hbtn_0e_0_usa
@@ -9,5 +10,5 @@ if __name__ == "__main__":
 
     db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
     cur = db.cursor()
-    cur.execute("SELECT * FROM 'states' WHERE name LIKE 'N%'")
+    cur.execute("SELECT * FROM `states` WHERE name LIKE 'N%'")
     [print(tup) for tup in cur.fetchall()]
