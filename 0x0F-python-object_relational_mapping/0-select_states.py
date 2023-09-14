fa9343531abf1a34662a9600cbfee4aa712465d6
Younis ahmed <younisalx-swe@hotmail.comgit config --global user.email younisalx-swe@hotmail.comgit config --global user.email younisalx-swe@hotmail.com>
2023-09-14 03:26:12 -0700

    task 0-1

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""
+lists all states from the database hbtn_0e_0_usa
+username, password, name will be provided as CMD arg
+"""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+
+    db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT * FROM 'states'")
+    [print(tup) for tup in cur.fetchall()]
---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,13 @@
+"""
+lists all states with a name starting with N (upper N)
+from the database hbtn_0e_0_usa
+"""
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+
+    db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT * FROM 'states' WHERE name LIKE 'N%'")
+    [print(tup) for tup in cur.fetchall()]
