a6f1765dfd22fc15c9647c04a8b66a126db9558c
Younis <younisalx-swe@hotmail.com>
2023-09-15 10:57:50 +0300

    task4

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -10,6 +10,9 @@ if __name__ == "__main__":
 
     db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
     cur = db.cursor()
-    cur.execute("SELECT * FROM `states` WHERE name=%s ORDER BY `id`", (argv[-1],))
+    cur.execute("SELECT * FROM
+                `states` WHERE name=%s
+                ORDER BY `id`",
+                (argv[-1],))
     [print(tup) for tup in cur.fetchall()]
     cur.close()
