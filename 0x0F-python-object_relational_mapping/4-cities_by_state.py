5e25da9cbd63e60994020ae38c150e9ae097e9f5
Younis <younisalx-swe@hotmail.com>
2023-09-15 11:55:18 +0300

    task5.1

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+takes in an argument and displays all values in the states\
+table of hbtn_0e_0_usa where name matches the argument
+"""
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+
+    db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
+    cur = db.cursor()
+    cur.execute("""SELECT cities.id, cities.name, states.name
+                FROM cities
+                JOIN states
+                ON cities.state_id = states.id
+                ORDER BY cities.id ASC""")
+    [print(tup) for tup in cur.fetchall()]
+    cur.close()
