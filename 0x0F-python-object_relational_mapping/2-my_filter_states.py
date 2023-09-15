caf454508f8e94af032ccb8a0d4d64f854fa2073
Younis <younisalx-swe@hotmail.com>
2023-09-15 10:36:40 +0300

    task3

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+takes in an argument and displays all values in the states\
+table of hbtn_0e_0_usa where name matches the argument
+"""
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+
+    db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
+    cur = db.cursor()
+    cur.execute(f"SELECT * FROM `states` WHERE name='{argv[4]}' ORDER BY `id`")
+    [print(tup) for tup in cur.fetchall()]
+    cur.close()
