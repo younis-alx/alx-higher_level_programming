36272ab3df8778ed37aea2292a8dc37ced6236cf
Younis ahmed <younisalx-swe@hotmail.comgit config --global user.email younisalx-swe@hotmail.comgit config --global user.email younisalx-swe@hotmail.com>
2023-09-14 22:09:26 -0700

    minor fixes

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -12,5 +12,5 @@ if __name__ == "__main__":
 
     db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
     cur = db.cursor()
-    cur.execute("SELECT * FROM 'states'")
+    cur.execute("SELECT * FROM `states`")
     [print(tup) for tup in cur.fetchall()]
