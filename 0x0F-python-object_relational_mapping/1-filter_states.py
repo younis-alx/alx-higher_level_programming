5768c57ab8270f6d7d5b4d690172951c3aa25b95
Younis <younisalx-swe@hotmail.com>
2023-09-15 10:30:20 +0300

    checker failure

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -8,7 +8,7 @@ from sys import argv
 
 if __name__ == "__main__":
 
-    db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3], host="localhost", port=3306)
+    db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
     cur = db.cursor()
     cur.execute("SELECT * FROM `states` WHERE name LIKE 'N%' ORDER BY `id`")
     [print(tup) for tup in cur.fetchall()]
