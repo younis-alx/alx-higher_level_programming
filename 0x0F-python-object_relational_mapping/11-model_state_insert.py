cbdb51226b0220f1d7da2ebd502389c3dbff95cb
Younis <younisalx-swe@hotmail.com>
2023-09-16 11:16:41 +0300

    task9,10,11

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+"""script that prints the first State object from the database"""
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sys import argv
+
+if __name__ == "__main__":
+    engine = create_engine(
+        f"mysql+mysqldb://{argv[1]}:{argv[2]}@localhost/{argv[3]}")
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    # try:
+    #     res = session.query(State).order_by(
+    #         State.id).filter(State.name == argv[4]).one()
+    #     print(res.id)
+    # except Exception:
+    #     print("Not found")
+    loui = State(name="Louisiana")
+    session.add(loui)
+    session.commit()
+    print(loui.id)
