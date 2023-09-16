1662ef46b3282ed55908353aab51edf58ccee263
Younis <younisalx-swe@hotmail.com>
2023-09-16 11:06:21 +0300

    task9,10

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+"""script that prints the first State object from the database"""
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sys import argv
+
+if __name__ == "__main__":
+    engine = create_engine(
+        f"mysql+mysqldb://{argv[1]}:{argv[2]}@localhost/{argv[3]}")
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    res = session.query(State).order_by(
+        State.id).filter(State.name.like('%a%')).all()
+    [print(i.id, i.name, sep=': ')
+     for i in res]
