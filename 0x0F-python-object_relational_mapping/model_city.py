b0cf490209ed8085aae7457b5337573e1658936f
Younis <younisalx-swe@hotmail.com>
2023-09-16 15:02:43 +0300

    last mandatory task

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -10,6 +10,6 @@ class City(Base):
     """City class with Base inheritance"""
     __tablename__ = 'cities'
     id = Column(Integer, autoincrement=True, unique=True,
-           nullable=False, primary_key=True)
+                nullable=False, primary_key=True)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
