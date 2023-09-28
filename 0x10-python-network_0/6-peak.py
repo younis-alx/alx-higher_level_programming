659483408c9eaccde2aa10286040429f059f7c5f
root <younisalx-swe@hotmail.com>
2023-09-28 07:07:26 -0700

    all but stubborn one

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -2,6 +2,8 @@
 """
 finds the peak in a list of unsorted int
 """
+
+
 def find_peak(list_of_integers):
     if not list_of_integers:
         return None
@@ -12,7 +14,8 @@ def find_peak(list_of_integers):
     while start < end:
         mid = (start + end) // 2
 
-        if list_of_integers[mid] > list_of_integers[mid - 1] and list_of_integers[mid] > list_of_integers[mid + 1]:
+        if list_of_integers[mid] > list_of_integers[mid - 1] and \
+           list_of_integers[mid] > list_of_integers[mid + 1]:
             return list_of_integers[mid]
         elif list_of_integers[mid] < list_of_integers[mid + 1]:
             start = mid + 1
