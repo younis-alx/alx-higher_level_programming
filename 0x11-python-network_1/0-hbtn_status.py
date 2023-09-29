96be986a2db23f72dc4d3a0af295861326fc28bb
jay-alx <126336909+younis-alx@users.noreply.github.com>
2023-09-29 10:59:58 +0000

    task 1

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" script that fetches https://alx-intranet.hbtn.io/status """
+
+import urllib.request
+
+
+if __name__ == "__main__":
+    url = "https://alx-intranet.hbtn.io/status"
+
+    with urllib.request.urlopen(url) as res:
+        res = res.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(res)))
+        print("\t- content: {}".format(res))
+        print("\t- utf8 content: {}".format(res.decode(encoding='utf-8')))
+
+
