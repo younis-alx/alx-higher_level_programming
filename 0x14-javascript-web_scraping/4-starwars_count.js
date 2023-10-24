dd944dfed061d715ddfccc01149b6efe548feb67
jay-alx <126336909+younis-alx@users.noreply.github.com>
2023-10-24 10:55:18 +0000

    count movies with ID 18

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -2,12 +2,12 @@
 const request = require('request');
 
 request(process.argv[2], (err, resp, body) => {
-    if (!err) {
-        const results = JSON.parse(body).results;
-        console.log(results.reduce((count, movie) => {
-            return movie.characters.find((character) => character.endsWith('/18/'))
-              ? count + 1
-              : count;
-          }, 0));
-        }
-      });
+  if (!err) {
+    const results = JSON.parse(body).results;
+    console.log(results.reduce((count, movie) => {
+      return movie.characters.find((character) => character.endsWith('/18/'))
+        ? count + 1
+        : count;
+    }, 0));
+  }
+});
